88946507ce53b61fdfc894d2800add575555f2ca
epili50 <epili50@gmail.com>
2024-05-09 20:39:14 +0200

    Hecho

--- js/app.js
+++ js/app.js
@@ -8,5 +8,36 @@ const pictures = [
 
 /**
  * ¿Qué variable de estado vamos a necesitar?
+
  * ¿Cómo se establece la imagen de fondo de un elemento mediante un estilo en línea?
  */
+
+let index = 0;
+
+// TODO: Seleccionar la foto del array y ponerla como imagen de fondo del contenedor del slider
+let container = document.querySelector('.img-container');
+updateSlider();
+function updateSlider(){
+container.style.backgroundImage = `url(${pictures[index]})`;
+console.log(pictures.length);
+}
+
+document.querySelector('.btn-right').addEventListener('click', () =>{
+  index++;
+  console.log(index);
+  if(index == pictures.length){
+    index = 0
+  }
+  updateSlider();
+  
+});
+
+document.querySelector('.btn-left').addEventListener('click', () =>{
+  index--; 
+  console.log(index);
+  if(index == -1){
+    index = pictures.length -1;
+  }
+  updateSlider();
+
+})
